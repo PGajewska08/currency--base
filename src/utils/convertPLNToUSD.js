587a81e9d63c41f9d6b5009847337da40320e51c
Paulina Gajewska <gajewska.paulina08@gmail.com>
2025-01-17 19:42:42 +0100

    Init and add first test

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,27 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if(typeof PLN === 'number' && PLN>0){
+    const PLNtoUSD = PLN / 3.5;
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
+  
+  else if(typeof PLN ==='string') {
+    return(NaN);
+  }
+  
+  else if ( PLN === undefined){
+    return(NaN);
+  }
+  
+  else if(PLN < 0){
+    return('$0.00');
+  }
+  
+  else {
+    return('Error');
+  }
+}
